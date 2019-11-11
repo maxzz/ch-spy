eba001bfb0b38628359f0744822916bf6e52ea72
Max Zakharzhevskiy <max.za@digitalpersona.com>
2019-11-10 23:19:11 -0800

    cannot import html

---
+++ src/components/GeneratedList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>CC {{title}} </div>
+</template>
+
+<script>
+    import { ref } from "@vue/composition-api";
+    //import html from '../assets/generated.html';
+    require('../assets/generated.html')
+    export default {
+        setup() {
+            const title = ref('title');
+            return {
+                title
+            };
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from 'vue';
+import store from './store';
+import VueCompositionApi from '@vue/composition-api';
+import VueHooks from '@u3u/vue-hooks';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueHooks);
+Vue.use(VueCompositionApi);
 
 new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    store,
+    render: h => h(App)
+}).$mount('#app');
