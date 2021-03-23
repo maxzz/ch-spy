395cf77d42e23c23e56215397a8bb14ef928f7f9
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-23 03:24:38 -0700

    Fix: add vite sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default ({command}) => {
     return defineConfig({
-        base: command === 'build' ? '/ch-spy/' : '',
+        //base: command === 'build' ? '/ch-spy/' : '',
+        base: '/',
         plugins: [vue()],
+        build: {
+            sourcemap: true
+        }
     });
 };
