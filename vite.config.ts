a24ef4c349feee637788ff354d05f9016955966f
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-23 01:46:42 -0700

    Fix: add proper deploy path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default ({command}) => {
     return defineConfig({
-        base: command === 'build' ? '/animated-sidebar/' : '',
+        base: command === 'build' ? '/ch-spy/' : '',
         plugins: [vue()],
     });
 };
