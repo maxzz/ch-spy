38993a81ec77c37341e39fb97933fd0cd30b45df
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-22 20:34:36 -0700

    Bug: cannot use module path with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import builtins from 'rollup-plugin-node-builtins';
+
+const builtinsPlugin = {
+    ...builtins({ crypto: true }),
+    name: 'rollup-plugin-node-builtins',
+  };
 
 // https://vitejs.dev/config/
 export default ({command}) => {
     return defineConfig({
         base: command === 'build' ? '/animated-sidebar/' : '',
-        plugins: [vue()],
+        plugins: [builtinsPlugin, vue()],
     });
 };
